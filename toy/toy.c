2530d48ef3e034e9c705d9fd6c0959169bb09a44
Edwin-Trejo <etrejo9@miners.utep.edu>
2020-12-02 14:46:59 -0700

    added many functions to toy.c, copied from shape-motion-demo movLayerDraw and mlAdvance, modified main to handle both new functions

--- toy/toy.c
+++ toy/toy.c
@@ -12,47 +12,179 @@
 
 short redrawScreen = 1;
 u_int fontFgColor = COLOR_GREEN;
+u_int bgColor = COLOR_WHITE;
+Region fieldFence;
 
 AbRect rect10 = {abRectGetBounds, abRectCheck, {10,10}}; /*10x10 rectangle  */
-AbRArrow rightArrow = {abRArrowGetBounds, abRArrowCheck, 30};
-
 AbRectOutline fieldOutline ={                 // playing field
   abRectOutlineGetBounds, abRectOutlineCheck,
   {screenWidth/2 - 10, screenHeight/2 - 10}
 };
 
+Layer layer4= {
+  (AbShape *)&circle8,
+  {40, (screenHeight/2)},
+  {0,0},{0,0},
+  COLOR_RED,
+  0
+ };
+
+Layer layer3= {
+  (AbShape *)&circle8,
+  {40, 30},
+  {0,0},{0,0},
+  COLOR_VIOLET,
+  &layer4,
+ };
+
+Layer fieldLayer = {
+  (AbShape *)&fieldOutline,
+  {screenWidth/2, screenHeight/2},
+  {0,0},{0,0},
+  COLOR_BLACK,
+  &layer3
+};
+
+Layer layer1= {
+  (AbShape *)&circle8,
+  {(screenWidth/2), 50},
+  {0,0},{0,0},
+  COLOR_ORANGE,
+  &fieldLayer,
+ };
+
+Layer layer0= {
+  (AbShape *)&rect10,
+  {(screenWidth/2), screenHeight-30},
+  {0,0},{0,0},
+  COLOR_BLUE,
+  &layer1,
+ };
+
+
+typedef struct MovLayer_s {
+  Layer *layer;
+  Vec2 velocity;
+  struct MovLayer_s *next;
+} MovLayer;
 
 
 
+/* initial value of {0,0} will be overwritten */
+MovLayer ml3 = { &layer4, {1,1}, 0 }; /**< not all layers move */
+MovLayer ml1 = { &layer1, {1,2}, &ml3 };
+MovLayer ml0 = { &layer3, {2,1}, &ml1 };
+
+
+void movLayerDraw(MovLayer *movLayers, Layer *layers)
+{
+  int row, col;
+  MovLayer *movLayer;
+
+  and_sr(~8);/**< disable interrupts (GIE off) */
+  for (movLayer = movLayers; movLayer; movLayer = movLayer->next) { /* for each moving layer */
+    Layer *l = movLayer->layer;
+    l->posLast = l->pos;
+    l->pos = l->posNext;
+  }
+
+  or_sr(8);/**< disable interrupts (GIE on) */
+
+  for (movLayer = movLayers; movLayer; movLayer = movLayer->next) { /* for each moving layer */
+    Region bounds;
+    layerGetBounds(movLayer->layer, &bounds);
+    lcd_setArea(bounds.topLeft.axes[0], bounds.topLeft.axes[1],
+		bounds.botRight.axes[0], bounds.botRight.axes[1]);
+    for (row = bounds.topLeft.axes[1]; row <= bounds.botRight.axes[1]; row++) {
+      for (col = bounds.topLeft.axes[0]; col <= bounds.botRight.axes[0]; col++) {
+	Vec2 pixelPos = {col, row};
+	u_int color = bgColor;
+	Layer *probeLayer;
+	for (probeLayer = layers; probeLayer;
+	     probeLayer = probeLayer->next) { /* probe all layers, in order */
+	  if (abShapeCheck(probeLayer->abShape, &probeLayer->pos, &pixelPos)) {
+	    color = probeLayer->color;
+	    break;
+	  } /* if probe check */
+	} // for checking all layers at col, row
+	lcd_writeColor(color);
+      } // for col
+    } // for row
+  } // for moving layer being updated
+}
+
+
+//Region fence = {{10,30}, {SHORT_EDGE_PIXELS-10, LONG_EDGE_PIXELS-10}}; /**< Crea
+void mlAdvance(MovLayer *ml, Region *fence)
+{
+  Vec2 newPos;
+  u_char axis;
+  Region shapeBoundary;
+
+  for (; ml; ml = ml->next) {
+    vec2Add(&newPos, &ml->layer->posNext, &ml->velocity);
+    abShapeGetBounds(ml->layer->abShape, &newPos, &shapeBoundary);
+    for (axis = 0; axis < 2; axis ++) {
+      if ((shapeBoundary.topLeft.axes[axis] < fence->topLeft.axes[axis]) ||
+	  (shapeBoundary.botRight.axes[axis] > fence->botRight.axes[axis]) ) {
+	int velocity = ml->velocity.axes[axis] = -ml->velocity.axes[axis];
+	newPos.axes[axis] += (2*velocity);
+      }/**< if outside of fence */
+    } /**< for axis */
+    ml->layer->posNext = newPos;
+  } /**< for ml */
+}
 
 void wdt_c_handler()
 {
   static int secCount = 0;
+  static short count = 0;
 
+  count ++;
   secCount ++;
+  
   if (secCount == 250) {		/* once/sec */
     secCount = 0;
     fontFgColor = (fontFgColor == COLOR_BLUE) ? COLOR_RED : COLOR_BLUE;
     redrawScreen = 1;
   }
+
+  if (count == 15){
+    mlAdvance(&ml0, &fieldFence);
+    if (p2sw_read())
+      redrawScreen = 1;
+    count =0;
+  }
 }
-  
+
+
 
 void main()
 {
   P1DIR |= LED_GREEN;		/**< Green led on when CPU on */		
   P1OUT |= LED_GREEN;
+  
   configureClocks();
   lcd_init();
+  shapeInit();
+  p2sw_init(1);
+
+  layerInit(&layer0);
+  layerDraw(&layer0);
+
+  layerGetBounds(&fieldLayer, &fieldFence);
+
   
   enableWDTInterrupts();      /**< enable periodic interrupt */
   or_sr(0x8);	              /**< GIE (enable interrupts) */
   
-  clearScreen(COLOR_WHITE);
+ 
+  
   while (1) {			/* forever */
     if (redrawScreen) {
       redrawScreen = 0;
-      drawString5x7(10,10, "Catch every ball!", fontFgColor, COLOR_WHITE);
+      drawString5x7(20,15, "Catch every ball!", fontFgColor, COLOR_WHITE);
+      movLayerDraw(&ml0, &layer0);
     }
     P1OUT &= ~LED_GREEN;	/* green off */
     or_sr(0x10);		/**< CPU OFF */
